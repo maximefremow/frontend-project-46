53df72835d3c4c9d39136d4d6402fec25129df52
maximefremow <114808664+maximefremow@users.noreply.github.com>
2024-03-26 21:40:10 +0300

    fix step1-3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .argument('<filepath1>', '<filepath2>')
   .action((filepath1, filepath2) => {
     console.log(parser(filepath1, filepath2));
   });
