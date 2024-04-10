88ce8a5db7b1894ce05b3b47f6ea8585de224f38
maximefremow <114808664+maximefremow@users.noreply.github.com>
2024-04-10 20:22:14 +0300

    fixstep4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import genDiff from '../src/genDiff.js';
 
 const program = new Command();
 
@@ -10,7 +10,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .argument('<filepath1>', '<filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2, 'json');
     console.log(diff);
